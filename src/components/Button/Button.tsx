d4efb7b53811d84c2bad1b73617b10a2329ba8af
a.ghanbari <a.ghanbari>
2024-07-29 16:46:24 +0330

    fjfnjewrf

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { useDispatch } from "react-redux";
+import { invoke } from "@tauri-apps/api";
+
+export interface ButtonProps {
+    reduxmethod:any,
+    reduxFiledName:any
+}
+
+const Button = (props: ButtonProps) => {
+    const dispatch = useDispatch();
+    React.useEffect(()=>{
+
+        const ipFetcher = async () =>{ 
+          const fetchedIp: any = await invoke("fetch_ip");
+        const responseURL = `http://${fetchedIp}:9090`;
+        dispatch(
+            props.reduxmethod([
+            [props.reduxFiledName],
+            [responseURL]
+          ])
+        );
+         }
+         ipFetcher();
+      },[])
+}
+
+export default Button;
---
+++ types/components/Button/Button.d.ts
@@ -0,0 +1,6 @@
+export interface ButtonProps {
+    reduxmethod: any;
+    reduxFiledName: any;
+}
+declare const Button: (props: ButtonProps) => void;
+export default Button;
